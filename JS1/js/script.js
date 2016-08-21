59096af7774c0bb64cd4f8c9bb54cb163f682162
GenaLee <graf.nuleen@gmail.com>
2016-08-21 22:53:26 +0300

    check input field for null

--- JS1/js/script.js
+++ JS1/js/script.js
@@ -1,93 +1,102 @@
-(function(){
-	var yourChoice = prompt('1. Возведение числа в степень \n2. Работа с массивом');
+(function() {
+    var yourChoice = prompt('1. Возведение числа в степень \n2. Работа с массивом');
 
 
-if (yourChoice == 1){ 
-	makePow();} 
-	else if (yourChoice == 2){
-		login();
-	}
-else{
-	alert('Для выбранного варианта нет действий');
-}
+    if (yourChoice == 1) {
+        makePow();
+    } else if (yourChoice == 2) {
+        login();
+    } else {
+        alert('Для выбранного варианта нет действий');
+    }
 
 
-function makePow(){
-var myNum = prompt('Введите целое число');
-var myPow = prompt('Введите степень числа (целое число)');
+    function makePow() {
+        var myNum = prompt('Введите целое число');
+        var myPow = prompt('Введите степень числа (целое число)');
 
-if (checkNum(myNum) & checkNum(myPow)){
-		console.log('Число '+ myNum + ' в степени ' + myPow +
-	  ' равно ' + myPowFunction(myNum, myPow));
- } 
+        if (myNum == '') {
+            console.log('Вы не ввели число!')
+        } else if (myPow == '') {
+            console.log('Вы не ввели степень!')
+        } else {
+            if (checkNum(myNum) & checkNum(myPow)) {
+                console.log('Число ' + myNum + ' в степени ' + myPow +
+                    ' равно ' + myPowFunction(myNum, myPow));
+            }
+        }
 
-function checkNum(numberForCheck){
+        function checkNum(numberForCheck) {
 
-if (isNaN(numberForCheck)) {
-	console.log('Вы ввели не число!');
-	return false;} else{
+            if (isNaN(numberForCheck)) {
+                console.log('Вы ввели не число!');
+                return false;
+            } else {
 
-  if (isInteger(numberForCheck) == false){
-	 console.log('Вы ввели не целое число!');
-	 return false;} else {return true;}
-}
+                if (isInteger(numberForCheck) == false) {
+                    console.log('Вы ввели не целое число!');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
 
-function isInteger(num) {
-	return (Math.ceil(num) - num == 0) 
-}
+            function isInteger(num) {
+                return (Math.ceil(num) - num == 0)
+            }
 
-}
+        }
 
-function myPowFunction(num, pow){
+        function myPowFunction(num, pow) {
 
-	var n;
-	if (pow == 0){
-		n = 1;
-	} else{
-	n = num;
-	var i = 1;
-	while (i < pow){
-		n *= num;
-		i++;
-	} 
-}
- if (pow < 0){
- 	n = 1 / n;
- }
-return n;
-}
+            var n;
+            if (pow == 0) {
+                n = 1;
+            } else {
+                n = num;
+                var i = 1;
+                while (i < pow) {
+                    n *= num;
+                    i++;
+                }
+            }
+            if (pow < 0) {
+                n = 1 / n;
+            }
+            return n;
+        }
 
-}
+    }
 
 
-function login(){
+    function login() {
 
-var nameStr = prompt('Введите пять любых имен через запятую');
-var nameArr = new Array(5);
-var myArr = new Array(5);
+        var nameStr = prompt('Введите пять любых имен через запятую');
+        var nameArr = new Array(5);
+        var myArr = new Array(5);
 
-nameArr = nameStr.split(',');
+        nameArr = nameStr.split(',');
 
-while (nameArr.length < 5){
-	nameArr.push(prompt('Недостаточно элементов. Введите еще!'));
-}
+        while (nameArr.length < 5) {
+            nameArr.push(prompt('Недостаточно элементов. Введите еще!'));
+        }
 
-for (var i=0; i < 5; i++){
-	myArr[i] = nameArr[i].trim().toUpperCase();
-}
+        for (var i = 0; i < 5; i++) {
+            myArr[i] = nameArr[i].trim().toUpperCase();
+        }
 
-var nameToFind = prompt('Введите имя пользователя:');
+        var nameToFind = prompt('Введите имя пользователя:');
 
-var msg = 'Ошибка! Такого пользователя не существует!';
-for (var i = 0; i < myArr.length; i++){
-	if (myArr[i] == nameToFind.toUpperCase()){
-		msg = nameToFind + ', Вы успешно вошли!';
-		break;
-	}
-}
+        var msg = 'Ошибка! Такого пользователя не существует!';
+        for (var i = 0; i < myArr.length; i++) {
+            if (myArr[i] == nameToFind.toUpperCase()) {
+                msg = nameToFind + ', Вы успешно вошли!';
+                break;
+            }
+        }
 
-	alert(msg);
-}
+        alert(msg);
+    }
 
 
 
